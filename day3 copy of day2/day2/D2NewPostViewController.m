3cb29e89b256eb6c20e4d56e423c6b1b65669b3e
John Clem <johnnyclem@gmail.com>
2013-09-12 10:57:52 -0700

    added a UIAlertView when creating a post without a userName

--- day3 copy of day2/day2/D2NewPostViewController.m
+++ day3 copy of day2/day2/D2NewPostViewController.m
@@ -9,7 +9,7 @@
 #import "D2NewPostViewController.h"
 #import "D2Post.h"
 
-@interface D2NewPostViewController ()
+@interface D2NewPostViewController () <UIAlertViewDelegate>
 {
     __weak IBOutlet UITextField *userNameTextField;
     __weak IBOutlet UITextField *titleTextField;
@@ -37,14 +37,56 @@
 
 -(IBAction) pressedSubmitButton
 {
+    if ([self isFormValid]) {
+        [self submitPost];
+    } else {
+        [self showAlertView];
+    }
+}
+
+- (void)submitPost
+{
     D2Post *post = [D2Post new];
     post.userName = userNameTextField.text;
     post.title = titleTextField.text;
     post.content = contentTextView.text;
     post.timeStamp = [NSDate new];
-
+    
     if( [_delegate respondsToSelector:@selector(newPostViewController:didCreateNewPost:)] )
         [_delegate newPostViewController:self didCreateNewPost:post];
+    
+}
+
+- (BOOL)isFormValid
+{
+    return userNameTextField.text.length;
+}
+
+#pragma mark - Alert View
+
+
+- (void)showAlertView
+{
+    UIAlertView *myAlertView = [[UIAlertView alloc] initWithTitle:@"Fields Are Blank"
+                                                          message:@"You Didn't Enter Any Text Into The Fields"
+                                                         delegate:self
+                                                cancelButtonTitle:@"Cancel"
+                                                otherButtonTitles:@"Post Anyway", nil];
+    [myAlertView show];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"Clicked Button: %ld", (long)buttonIndex);
+    
+    switch (buttonIndex) {
+        case 1:
+            [self submitPost];
+            break;
+        default:
+            // don't do anything, dismiss
+            break;
+    }
 }
 
 @end
