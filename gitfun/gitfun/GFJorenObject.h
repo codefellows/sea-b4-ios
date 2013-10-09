bb01c0cbb0f0d00f2cc2c157e88809fd61aeb255
Joren Winge <viperxj7@gmail.com>
2013-10-09 09:41:25 -0700

    added the joren object

---
+++ gitfun/gitfun/GFJorenObject.h
@@ -0,0 +1,13 @@
+//
+//  GFJorenObject.h
+//  gitfun
+//
+//  Created by Joren Winge on 10/9/13.
+//  Copyright (c) 2013 General UI, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GFJorenObject : NSObject
+
+@end
