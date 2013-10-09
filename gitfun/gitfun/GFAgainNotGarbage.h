5bbd8a213c73aed8779c60a7a01f47a2009d2c57
John Bender <john@generalui.com>
2013-10-09 09:58:35 -0700

    new file again

---
+++ gitfun/gitfun/GFAgainNotGarbage.h
@@ -0,0 +1,13 @@
+//
+//  GFAgainNotGarbage.h
+//  gitfun
+//
+//  Created by John Bender on 10/9/13.
+//  Copyright (c) 2013 General UI, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GFAgainNotGarbage : NSObject
+
+@end
