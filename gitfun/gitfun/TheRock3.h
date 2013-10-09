e8806c3bd13639e29e60bfd8f9a8ebb41a3c03ea
brad <johnson.bradley01@gmail.com>
2013-10-09 10:10:25 -0700

    The Rock was here

---
+++ gitfun/gitfun/TheRock3.h
@@ -0,0 +1,13 @@
+//
+//  TheRock3.h
+//  gitfun
+//
+//  Created by Brad on 10/9/13.
+//  Copyright (c) 2013 General UI, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TheRock3 : NSObject
+
+@end
