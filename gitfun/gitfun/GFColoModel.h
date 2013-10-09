d1c8b383ab15475e6043b682c9e5d6f42d4a798c
colo <rfcolome@gmail.com>
2013-10-09 09:41:42 -0700

    this is my new object

---
+++ gitfun/gitfun/GFColoModel.h
@@ -0,0 +1,13 @@
+//
+//  GFColoModel.h
+//  gitfun
+//
+//  Created by Ricardo Colome on 10/9/13.
+//  Copyright (c) 2013 General UI, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GFColoModel : NSObject
+
+@end
