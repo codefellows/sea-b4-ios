7602e54f6a1ad75d38b0c4724a4e4f5545b4063a
Jason Koceja <jason@koceja.com>
2013-10-09 09:40:12 -0700

    your mom

--- gitfun/gitfun/GFAppDelegate.m
+++ gitfun/gitfun/GFAppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    NSLog(@"These are not the droid we're looking for");
     return YES;
 }
 							
