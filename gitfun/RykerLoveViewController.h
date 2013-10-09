5287a937501cc931f76041c1ec48b7a2d134582a
Daniel Sperry <dsperry@Daniels-MacBook-Pro.local>
2013-10-09 10:01:35 -0700

    Trekky commit for christian

---
+++ gitfun/RykerLoveViewController.h
@@ -0,0 +1,13 @@
+//
+//  RykerLoveViewController.h
+//  gitfun
+//
+//  Created by Daniel Sperry on 10/9/13.
+//  Copyright (c) 2013 General UI, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RykerLoveViewController : UIViewController
+
+@end
