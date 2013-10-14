735bd980a0cfd10e643626ae3028b6d13e26ebf1
John Bender <john@generalui.com>
2013-10-14 15:53:36 -0700

    array functions

--- memory/memory/MMViewController.m
+++ memory/memory/MMViewController.m
@@ -13,7 +13,7 @@
 
 
 
-void functionToCall( int *h, int *s, int *v )
+void functionToCall( int *h, int *s, int *v, int *array )
 {
     printf( "in function &h %x &s %x &v %x\n", &h, &s, &v );
     printf( "in function, h %x s %x v %x\n", h, s, v );
@@ -21,6 +21,10 @@ void functionToCall( int *h, int *s, int *v )
     *h = 14;
     *s = 15;
     *v = 16;
+
+    *array = 103;
+    *(array + 1) = 104;
+    *(array + 18) = 109; // yikes! wrote somebody else's memory and didn't crash!!
 }
 
 void caller()
@@ -28,9 +32,19 @@ void caller()
     int h = 0, s = 1, v = 2;
     printf( "h %d s %d v %d\n", h, s, v );
     printf( "&h %x &s %x &v %x\n", &h, &s, &v );
-    functionToCall( &h, &s, &v );
-    printf( "h %d s %d v %d\n", h, s, v );
+
+    int *arr = malloc( 8*sizeof( int ) );
+    arr[0] = 5;
+    arr[1] = 13;
+    *(arr + 2) = 91;
+    printf( "arr[0]=%d, arr[1]=%d, arr[2]=%d\n", arr[0], arr[1], arr[2] );
+
+    functionToCall( &h, &s, &v, arr );
+
+    printf( "after: h %d s %d v %d\n", h, s, v );
+    printf( "after: arr[0]=%d, arr[1]=%d, arr[2]=%d\n", arr[0], arr[1], arr[2] );
 }
+
 
 @interface MMViewController ()
 
