49283860b98d24c8b08ab9ef34e3541d1b24f034
John Clem <johnnyclem@gmail.com>
2013-10-17 10:59:26 -0700

    added simple demo project for adding a scroll view in Interface Builder using Auto Layout

---
+++ Scroll View/Scroll View/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Scroll View
+//
+//  Created by John Clem on 10/17/13.
+//  Copyright (c) 2013 Code Fellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JCAppDelegate class]));
+    }
+}
